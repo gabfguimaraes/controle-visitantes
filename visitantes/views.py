0b591f9ac1c66a4b447e9413c28fa07f7aae1a80
Gabriel Freitas <gabrielfreitasg2@gmail.com>
2024-03-25 16:14:42 -0300

    Add pagina Informações de Visitantes

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from visitantes.forms import VisitanteForm
+from visitantes.models import Visitante
+
 
 def registrar_visitante(request):
 
@@ -28,3 +30,14 @@ def registrar_visitante(request):
     }
 
     return render(request, "registrar_visitante.html", context)
+
+def informacoes_visitante(request, id):
+
+    visitante = get_object_or_404(Visitante, id=id)
+    
+    context = {
+        "nome_pagina": "Informações de visitante",
+        "visitante": visitante
+    }
+    
+    return render(request, "informacoes_visitante.html", context)
