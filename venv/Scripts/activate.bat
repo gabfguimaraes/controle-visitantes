255c4122a3a0ae725a9885c35a8eaae4c9a88df9
Gabriel <gabrielfreitasg2@gmail.com>
2024-04-27 18:18:52 -0300

    add gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\gabriel.fguimaraes\Projetos\GitHub\controle-visitantes\venv
+set VIRTUAL_ENV=C:\Users\Gabriel\Projetos\Github\controle-visitantes\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
