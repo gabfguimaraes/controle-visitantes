86520fb77a85dd58be811e33a431004db0744abf
Gabriel <gabrielfreitasg2@gmail.com>
2024-04-28 21:02:47 -0300

    Inserido login e logout

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 from dashboard.views import index
 import visitantes.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html"
+        ),
+        name="login"
+    ),
+    
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(
+            template_name="logout.html"
+        ),
+        name="logout"
+    ),
 
     path(
         "", 
